b122a46f87812f66704e6399a168b02be6657609
Lsepman <leticiasepulvedamancilla@gmail.com>
2025-01-16 18:42:58 +0100

    Subiendo cambios recientes

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable, Output } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import  {Injectable} from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +9,24 @@ export class GifsService {
 
   //gestiona el historial de gifs
   private _historialEtiqueta: string[] = [];
-  private apiKey: string= '0wlU1oJQfUnBbaubHYRTtJW8LJ776C6V';
 
+  //Aqui creamos una propiedad con la url de la api y el key
+  private apiKey: string= '0wlU1oJQfUnBbaubHYRTtJW8LJ776C6V';
+  private serviceUrl: string= 'https://api.giphy.com/v1/gifs';
+
+  //Aqui se inyecta el servicio HttpClient dentro del servicio
+  constructor(private http: HttpClient){
+
+  }
 
   //Y creamos un get para acceder al dato. Lo creamos como privado para evitar que se modifique el array accidentalmente. De hecho, utilizamos el spread (...) para evitar que se cree una referencia al dato al realizar el return.
   get historialEtiquetas(){
     return [...this._historialEtiqueta];
   }
 
-  constructor() { }
-
 
   //Metodo para añadir las nuevas etiquetas al array en la primera posicion.
-  buscarEtiqueta(etiqueta: string) : void{
+  ordenarHistorial(etiqueta: string) : void{
       etiqueta = etiqueta.toLowerCase().trim();
       if(etiqueta == ""){
         window.alert("No puedes introducir vacio");
@@ -35,6 +41,22 @@ export class GifsService {
           this._historialEtiqueta.pop();
       }
       this._historialEtiqueta.unshift(etiqueta);
+
+  }
+
+
+  buscarEtiqueta(etiqueta: string): void{
+    this.ordenarHistorial(etiqueta);
+    // Sirve para pasar los parametros de busqueda 
+    const params = new HttpParams()
+    .set('api_key', this.apiKey)
+    .set('limit',10)
+    .set('q', etiqueta);
+
+    //Esto es un observable, que consiste en un objeto que emite diferentes valores a lo largo del tiempo. Esto quiere decir que esta escuchando las emisiones que el objeto esta emitiendo.
+    this.http.get(`${ this.serviceUrl}/search`, {params}).subscribe( resp =>{console.log(resp);
+
+    })
 
   }
 }
