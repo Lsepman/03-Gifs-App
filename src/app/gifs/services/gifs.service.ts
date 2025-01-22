1a54fbea587080b195365a0edc3c4dbd3a5679a1
Lsepman <leticiasepulvedamancilla@gmail.com>
2025-01-22 17:17:04 +0100

    Subiendo cambios recientes

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,6 +9,9 @@ import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 })
 export class GifsService {
 
+  constructor(private http: HttpClient){
+    this.cargarLocalStorage();
+  }
   public listadoGifs: Gif[]= [];
 
   //Array para mantener un historia de las etiquetas de busqueda realizadas. _ quiere decir que es privado
@@ -18,14 +21,21 @@ export class GifsService {
   private apiKey: string= '0wlU1oJQfUnBbaubHYRTtJW8LJ776C6V';
   private serviceUrl: string= 'https://api.giphy.com/v1/gifs';//URL base de la API de Giphy
 
+  //Metodo para almacenar los datos en LocalStorage
+  private almacenarLocalStorage(){
+    localStorage.setItem('historial', JSON.stringify(this._historialEtiqueta)) // strigify convierte un array a string para guardarlo como un JSON
+  }
+  private cargarLocalStorage() : void{
+    if(!localStorage.getItem('historial')) return; //Aqui le indicamos que si el almacen esta vacio salga
+    this._historialEtiqueta = JSON.parse(localStorage.getItem('historial')!); //va a devolver un string o null, el JSON.parse convierte el string en un array, y con la exclamacion indicamos que no va a venir nunca un null
 
-
+    if(this._historialEtiqueta.length === 0)return; //Se puede omitir porque en el primer if ya estamos obligando a que si esta vacio salga.
+    this.buscarEtiqueta(this._historialEtiqueta[0]); //aqui cogemos las etiquetas que buscamos primera y que en el array ocupa el lugar 0
+}
 
 
   //Constructor del servicio, se inyecta HttpClient en el construtor habilitando el uso del cliente HTTP en este servicio
-  constructor(private http: HttpClient){
 
-  }
 
   //Proporciona acceso a una copia del historial de etiquetas. (...) Devuelve una copia del array para evitar que se modifique el original accidentalmente desde fuera del servicio.
   get historialEtiquetas(){
@@ -52,6 +62,7 @@ export class GifsService {
       }
       //Agrega la nueva etiqueta al principio del historial
       this._historialEtiqueta.unshift(etiqueta);
+      this.almacenarLocalStorage();
 
   }
 
@@ -65,12 +76,13 @@ export class GifsService {
     .set('q', etiqueta);
 
     //Esto es un observable, que consiste en un objeto que emite diferentes valores a lo largo del tiempo. Esto quiere decir que esta escuchando las emisiones que el objeto esta emitiendo.
-    //Envia una solicitud GET a la API de Giphy usando los parametros y la URL base. suscribe escucha la respuesta de la API de forma asincrona. 
+    //Envia una solicitud GET a la API de Giphy usando los parametros y la URL base. suscribe escucha la respuesta de la API de forma asincrona.
     this.http.get<SearchResponse>(`${ this.serviceUrl}/search`, {params}).subscribe( resp =>{
       this.listadoGifs = resp.data;
       console.log({gifs : this.listadoGifs});
 
-    })
+    });
+
 
   }
 }
